34eee84671a6cd5921c4ff839b979a78f1b11614
pablofrizzi <pablofrizzi@gmail.com>
2017-02-20 19:53:30 +0100

    event.target added

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-var header = $('h2');
-
 function classToggleEventHandler(event) {
     var target = $(event.target);
     if (target.is('h2')) {
-        header.parent()
+        target.parent()
             .find('.article-content')
             .toggleClass('hidden');
         console.log(target);
     }
 }
 
-header.click(classToggleEventHandler);
+$('.text-box-container').click(classToggleEventHandler);
 
